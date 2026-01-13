b23f12bc7fd94b4dc3e0c7113c0f8688428b4e2c
n0signull <102259497+n0signull@users.noreply.github.com>
2026-01-13 16:52:53 -0500

    Create outputs.py

---
+++ controller/outputs.py
@@ -0,0 +1,10 @@
+import logging
+
+class OutputManager:
+    def update(self, state):
+        if state == "LOCKED":
+            logging.info("Outputs: LOCKED (relay OFF, LED RED)")
+        elif state == "IN_PROGRESS":
+            logging.info("Outputs: IN PROGRESS (LED YELLOW)")
+        elif state == "SOLVED":
+            logging.info("Outputs: SOLVED (relay ON, LED GREEN)")
