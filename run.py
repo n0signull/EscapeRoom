b739de1ed5994fd520c3aff1f453ae19fdb61a04
n0signull <102259497+n0signull@users.noreply.github.com>
2026-01-13 17:07:55 -0500

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ from controller.main import loop
 from web.app import app
 
 threading.Thread(target=loop, daemon=True).start()
-app.run(host="0.0.0.0", port=5000)
-
+app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
