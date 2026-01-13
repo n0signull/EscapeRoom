2fc8a213116fe66ed665b1010dec4aad88f69476
n0signull <102259497+n0signull@users.noreply.github.com>
2026-01-13 16:56:17 -0500

    Update main.py

--- controller/main.py
+++ controller/main.py
@@ -1,1 +1,21 @@
+import yaml
+import time
+import logging
+from controller.puzzle import PuzzleEngine
+from controller.inputs import InputManager
+from controller.outputs import OutputManager
+
+logging.basicConfig(level=logging.INFO)
+
+with open("controller/config.yaml") as f:
+    config = yaml.safe_load(f)
+
+inputs = InputManager()
+outputs = OutputManager()
+engine = PuzzleEngine(config, inputs, outputs)
+
+def loop():
+    while True:
+        engine.update()
+        time.sleep(0.1)
 
